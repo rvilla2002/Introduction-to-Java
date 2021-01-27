4dd5506f1d55781b5136a0c70ef960d0a6cf36b5
Rishi V <78003113+rvilla2002@users.noreply.github.com>
2021-01-26 18:06:28 -0600

    Add files via upload

---
+++ Unit 01- First Lab/FirstLab.java
@@ -0,0 +1,57 @@
+/** 
+ * The FirstLab class <replace this with description of your program>
+ *
+ * @author <FirstName> <LastName>
+ * Collaborators: <None OR name of student(s) that you worked with>
+ * Teacher Name: 
+ * Period:
+ * Due Date:
+ */
+
+public class FirstLab 
+{
+	public static void main(String[] args) 
+	{
+		// Write the code HERE to print your name and address in boxes
+		System.out.println("+*****************+");
+		System.out.println("|   Rishi Villa   |");
+		System.out.println("+*****************+");
+		System.out.println("|  6000 Ohio dr   |");
+		System.out.println("+*****************+");
+        System.out.println("|    Apt 1616     |");
+        System.out.println("+*****************+");
+		System.out.println("|  Plano, Texas   |");
+		System.out.println("+*****************+");
+		System.out.println("|     75093       |");
+		System.out.println("+*****************+");
+        System.out.println("                       ");	
+        System.out.println("                       ");			
+		System.out.println("                       ");	
+	    System.out.println("                       ");		
+	    System.out.println("  ################     ");	
+	    System.out.println("#  ----      ----  #   ");
+	    System.out.println("#   ^^        ^^   #   ");
+	    System.out.println("#                  #   ");
+	    System.out.println("#        @@        #   ");
+	    System.out.println("#                  #   ");
+	    System.out.println("# &              & #   ");
+	    System.out.println("#  &            &  #   ");
+	    System.out.println("#   &          &   #   ");
+	    System.out.println("#    & & & & &     #   ");
+	    System.out.println("  ################     ");
+  	
+		
+		
+		// This calls the bonus method to draw your silly face/picture
+		bonus();
+	}
+	
+	/** Draws a silly face or picture
+	 */	
+	public static void bonus()
+	{
+		// Write the code HERE to display a silly face or picture
+		
+		
+	}
+}
